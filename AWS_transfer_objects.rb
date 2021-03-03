8153c6b719557fcc8aa61a2a4700fc9fe99ead98
Jkim5057 <79962391+Jkim5057@users.noreply.github.com>
2021-03-03 14:04:05 -0500

    Update AWS_transfer_objects.rb

--- AWS_transfer_objects.rb
+++ AWS_transfer_objects.rb
@@ -18,19 +18,13 @@ s3 = Aws::S3::Client.new(region: '--region--')        #<---------- Enter region 
 
 resp=s3.copy_object(bucket: to_bucket,copy_source: "#{bucket_origin}/#{obj_key}",key: obj_key)
 puts "Sending #{obj_key} from #{bucket_origin} to #{bucket_destination}"
-MyLog.log.info "Sending #{obj_key} from #{bucket_origin} to #{bucket_destination}"
 puts "Response: #{resp}"
-MyLog.log.info "Response: #{resp}"
 puts "Send Object(s) to Bucket (#{bucket_destination}):"
-MyLog.log.info "Object(s) sent to Bucket (#{bucket_destination}):"
 s3.list_objects({bucket: bucket_destination,}).contents.each do |item|
     puts " Name: #{item.key}"
-    MyLog.log.info " Name: #{item.key}"
 end
 puts ""
 puts "Objects from bucket (#{bucket_origin}):"
-MyLog.log.info "Object from bucket (#{bucket_origin}):"
 s3.list_objects({bucket: bucket_origin,}).contents.each do |item|
     puts " Name: #{item.key}"
-    MyLog.log.info " Name: #{item.key}"
 end
