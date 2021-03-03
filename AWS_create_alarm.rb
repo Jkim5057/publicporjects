543422c093b20df0d089c9552592ea438fc91d36
Jkim5057 <79962391+Jkim5057@users.noreply.github.com>
2021-03-03 13:41:20 -0500

    Update AWS_create_alarm.rb

--- AWS_create_alarm.rb
+++ AWS_create_alarm.rb
@@ -12,13 +12,13 @@ require 'aws-sdk-resources'
 accountnum='--#--'          #<----------enter in your account number
 bucketname="-----"     #<----------enter in your S3 bucket name
 topicname="-----"       #<----------enter in your SNS endpoint
+regionname="-----"       #<----------enter in your region where your bucket resides
 
-
-arnobj = Aws::ARN.new(partition: 'aws', service: 'sns', region: 'us-east-1',account_id: accountnum, resource: topicname)
+arnobj = Aws::ARN.new(partition: 'aws', service: 'sns', region: regionname ,account_id: accountnum, resource: topicname)
 topicarn = arnobj.to_s()
 puts "Topic ARN: #{topicarn}"
 MyLog.log.info"Topic ARN: #{topicarn}"
-cw = Aws::CloudWatch::Client.new(region: 'your region') #<----------enter in your region
+cw = Aws::CloudWatch::Client.new(region: regionname) 
 alarm_name = "Frequent Commits"
 
 resp=cw.put_metric_alarm({
