ff9693afc4c2d1a9f629cec91b91a8901ddab062
Jkim5057 <79962391+Jkim5057@users.noreply.github.com>
2021-03-03 13:38:29 -0500

    Create AWS_transfer_objects.rb
    #Simple object transfer across buckets of your choice

---
+++ AWS_transfer_objects.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+require 'aws-sdk-s3'
+require 'awesome_print'
+
+
+bucket_origin = '--origin--'                          #<---------- Enter bucket of origin
+bucket_destination = '--destination--'                #<---------- Enter bucket destination
+obj_key = '--name of file(s)--'                       #<---------- Enter object name (file name)
+s3 = Aws::S3::Client.new(region: '--region--')        #<---------- Enter region name of buckets
+
+
+
+
+resp=s3.copy_object(bucket: to_bucket,copy_source: "#{bucket_origin}/#{obj_key}",key: obj_key)
+puts "Sending #{obj_key} from #{bucket_origin} to #{bucket_destination}"
+MyLog.log.info "Sending #{obj_key} from #{bucket_origin} to #{bucket_destination}"
+puts "Response: #{resp}"
+MyLog.log.info "Response: #{resp}"
+puts "Send Object(s) to Bucket (#{bucket_destination}):"
+MyLog.log.info "Object(s) sent to Bucket (#{bucket_destination}):"
+s3.list_objects({bucket: bucket_destination,}).contents.each do |item|
+    puts " Name: #{item.key}"
+    MyLog.log.info " Name: #{item.key}"
+end
+puts ""
+puts "Objects from bucket (#{bucket_origin}):"
+MyLog.log.info "Object from bucket (#{bucket_origin}):"
+s3.list_objects({bucket: bucket_origin,}).contents.each do |item|
+    puts " Name: #{item.key}"
+    MyLog.log.info " Name: #{item.key}"
+end
